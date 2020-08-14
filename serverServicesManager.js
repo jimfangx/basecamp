13725498a29270eee75906c95dc4baf3a5dc35d1
AirFusion <jfang.cv.ca.us@gmail.com>
2020-08-14 13:09:00 -0700

    start working on server service distribution system

---
+++ serverServicesManager.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const { response } = require('express');
+var bodyParser = require('body-parser')
+var app = express()
+    // bodyParser = require('body-parser');
+
+// https://stackoverflow.com/questions/9177049/express-js-req-body-undefined#
+
+// app.use(bodyParser.json());
+app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+app.post('/', function (req, res) {
+    // console.log(req.body.paradigm)
+    let paradigmScrape = require('./judgeAnalysis')
+    let paradigmResults =  paradigmScrape(req.body.paradigm)
+    // response.setHeader('Content-Type', 'text/html');
+    console.log(paradigmResults)
+    res.send(paradigmResults)
+})
+
+
+// port = process.env.PORT;
+// if (port == null || port == "") {
+//     port = 8000;
+// }
+
+app.listen('8080')
+console.log(`Listening at localhost:8000`)
