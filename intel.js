28f5df113c763ecdb76099a5c15d60cc30523c6e
AirFusion <jfang.cv.ca.us@gmail.com>
2020-10-27 01:14:11 -0700

    neg on intel almost working... need to account for a few special cases

--- intel.js
+++ intel.js
@@ -5,10 +5,11 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto("https://hspolicy.debatecoaches.org/Casady%20School/Burger-Fryer%20Aff");
+    let link = "https://hspolicy.debatecoaches.org/Coppell/Patel-Yan%20Neg"
+    await page.goto(link);
     await page.waitFor(500);
-    
-    if ("https://hspolicy.debatecoaches.org/Casady%20School/Burger-Fryer%20Aff".includes("Aff")) { //its aff intel
+
+    if (link.includes("Aff")) { //its aff intel
         let affReturnData = {
             titles: [],
             planText: []
@@ -16,7 +17,7 @@ const puppeteer = require('puppeteer');
         // let pageHTML = response.text().toLowerCase()
         affReturnData.titles = await page.evaluate(() => {
             let titles = []
-            for(i=2; i<=document.querySelector("#tblCites > tbody").rows.length; i++) {
+            for (i = 2; i <= document.querySelector("#tblCites > tbody").rows.length; i++) {
                 titles.push(document.querySelector(`#tblCites > tbody >tr:nth-child(${i}) > td:nth-child(1) > div > h4`).innerText)
             }
             return titles
@@ -26,7 +27,47 @@ const puppeteer = require('puppeteer');
         // while (pageHTML.includes(`the united states federal government should `)) {
         //     let planText = pageHTML.indexOf(`the united states federal government should`)-1
         // }
-        
+
+    } else if (link.includes('Neg')) {
+        let negReturnData = {
+            rawList: [],
+            cp: [],
+            da: [],
+            k: [],
+            t: []
+        }
+
+        negReturnData.rawList = await page.evaluate(() => {
+            let titles = []
+            for (i = 2; i <= document.querySelector("#tblCites > tbody").rows.length; i++) {
+                titles.push(document.querySelector(`#tblCites > tbody >tr:nth-child(${i}) > td:nth-child(1) > div > h4`).innerText.toLowerCase())
+            }
+            return titles
+        })
+        console.log(negReturnData)
+
+        for (i = 0; i < negReturnData.rawList.length; i++) {
+            if (negReturnData.rawList[i].includes('cp ') || negReturnData.rawList[i].includes('cp-') || negReturnData.rawList[i].includes('cp:')) {
+                negReturnData.cp.push(negReturnData.rawList[i])
+            }
+            else if (negReturnData.rawList[i].includes('da ') || negReturnData.rawList[i].includes('da-') || negReturnData.rawList[i].includes('da:')) {
+                negReturnData.da.push(negReturnData.rawList[i])
+            }
+            else if (negReturnData.rawList[i].includes('k ') || negReturnData.rawList[i].includes('k-') || negReturnData.rawList[i].includes('k:')) {
+                if (!negReturnData.rawList[i].includes('da ') && !negReturnData.rawList[i].includes('da-') && !negReturnData.rawList[i].includes('da:')) {
+                    negReturnData.k.push(negReturnData.rawList[i])
+                }
+            } else if (negReturnData.rawList[i].includes('t ') || negReturnData.rawList[i].includes('t-') || negReturnData.rawList[i].includes('t:')) {
+                if (!negReturnData.rawList[i].includes('da ') && !negReturnData.rawList[i].includes('da-') && !negReturnData.rawList[i].includes('da:')) {
+                    if (!negReturnData.rawList[i].includes('k ') && !negReturnData.rawList[i].includes('k-') && !negReturnData.rawList[i].includes('k:')) {
+                        negReturnData.t.push(negReturnData.rawList[i])
+                    }
+                }
+            }
+        }
+        console.log(negReturnData)
+
+        // todo: dedev (case turns aka it), p (theory) https://hspolicy.debatecoaches.org/Chaminade/Barsoumian-Kim%20Neg && replacee duplicates/ diff veersions
     }
 
 })();
