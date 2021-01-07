2877c31fbbaaa6f097401d5bfd07243e67f937b4
AirFusion <jfang.cv.ca.us@gmail.com>
2021-01-07 00:46:17 -0800

    add support for current active rounds display

--- frontend/main.js
+++ frontend/main.js
@@ -44,40 +44,65 @@ ipcRenderer.on('tabroomAuthSuccessful', (event, data) => {
             // upcoming/current tournament
             //request for now tournaments, if not then exec below code
             superagent
-                .get('https://tabroomapi.herokuapp.com/me/future')
+                .get('https://tabroomapi.herokuapp.com/me/current')
+                // .get('http://localhost:8080/me/current') // Forcing active round by loading a html file of an active round
                 .set('Content-Type', 'application/x-www-form-urlencoded')
                 .send(JSON.parse(`{"apiauth":"${config.tabroomAPIKey}", "token":"${authCredentials.token}"}`))
                 .end((err, res) => {
 
-                    if (res.statusCode == 200) { // user has a future entry
-                        var dateArray = []
-                        var earlistElementNumber = 0
-                        for (i = 0; i < res.body.length; i++) {
-                            dateArray.push(Date.parse(res.body[i].date))
-                        }
-                        dateArray.sort((a, b) => a - b)
+                    if (res.statusCode == 204) { // if no current rounds active
 
-                        for (i = 0; i < res.body.length; i++) {
-                            if (dateArray[0] === Date.parse(res.body[i].date)) {
-                                earlistElementNumber = i
-                            }
-                        }
+                        superagent
+                            .get('https://tabroomapi.herokuapp.com/me/future')
+                            .set('Content-Type', 'application/x-www-form-urlencoded')
+                            .send(JSON.parse(`{"apiauth":"${config.tabroomAPIKey}", "token":"${authCredentials.token}"}`))
+                            .end((err, res) => {
 
-                        $('#mainTag').text(``).css('margin-top',"0%")
-                        $('#tournamentName').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Tournament:</a> ${res.body[earlistElementNumber].name}`)
-                        $('#date').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Date:</a> ${res.body[earlistElementNumber].date}`)
-                        $('#event').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Division:</a> <a href="${res.body[earlistElementNumber].eventLink}" target="_blank" style="text-decoration: underline; color:black"">${res.body[earlistElementNumber].event}</a>`)
-                        $('#location').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Location:</a> ${res.body[earlistElementNumber].location}`)
-                        $('#status').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Status:</a> ${res.body[earlistElementNumber].status}`)
-                        $('#prefs').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Pref Info:</a> ${res.body[earlistElementNumber].prefs}`)
-                        $('#info').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Additional Info:</a> ${res.body[earlistElementNumber].info}`)
-                        $('#notes').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Additional Notes:</a> ${res.body[earlistElementNumber].notes}`)
+                                if (res.statusCode == 200) { // user has a future entry
+                                    var dateArray = []
+                                    var earlistElementNumber = 0
+                                    for (i = 0; i < res.body.length; i++) {
+                                        dateArray.push(Date.parse(res.body[i].date))
+                                    }
+                                    dateArray.sort((a, b) => a - b)
 
-                        console.log(res.body[earlistElementNumber])
+                                    for (i = 0; i < res.body.length; i++) {
+                                        if (dateArray[0] === Date.parse(res.body[i].date)) {
+                                            earlistElementNumber = i
+                                        }
+                                    }
+
+                                    $('#mainTag').text(``).css('margin-top', "0%")
+                                    $('#tournamentName').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Tournament:</a> ${res.body[earlistElementNumber].name}`)
+                                    $('#date').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Date:</a> ${res.body[earlistElementNumber].date}`)
+                                    $('#event').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Division:</a> <a href="${res.body[earlistElementNumber].eventLink}" target="_blank" style="text-decoration: underline; color:black"">${res.body[earlistElementNumber].event}</a>`)
+                                    $('#location').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Location:</a> ${res.body[earlistElementNumber].location}`)
+                                    $('#status').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Status:</a> ${res.body[earlistElementNumber].status}`)
+                                    $('#prefs').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Pref Info:</a> ${res.body[earlistElementNumber].prefs}`)
+                                    $('#info').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Additional Info:</a> ${res.body[earlistElementNumber].info}`)
+                                    $('#notes').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Additional Notes:</a> ${res.body[earlistElementNumber].notes}`)
+
+                                    console.log(res.body[earlistElementNumber])
+
+                                }
+                                // currently working on the now tournaments thing in the api
+                                // need xwiki api too
+                            })
+                    } else if (res.statusCode == 200) { // there is a current round active (update the front page info)
+
+                        $('#mainTag').text(``).css('margin-top', "0%")
+                        $('#oppoent').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Oppoent:</a> ${res.body[1].oppoent}`) // [1] becuase [0] is the tournament info, [1] is the info of the first round
+                        $('#judge').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Judge:</a> <a href="${res.body[1].paradigmLink}" target="_blank" style="text-decoration: underline; color:black"">${res.body[1].judge}</a>`)
+                        $('#room').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Room:</a> ${res.body[1].room}`)
+                        var todayDate = new Date()
+                        todayDate = todayDate.toDateString()
+
+                        $('#datesAndStartTimes').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Room:</a> ${todayDate} ${res.body[1].startTime}`)
+                        $('#round').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Round:</a> ${res.body[1].roundNum}`)
+                        $('#codeAndEvent').html(`<a class="fs-4 text-muted" style="text-decoration: none;">Code & Event:</a> ${res.body[0].code} ${res.body[0].event}`)
 
                     }
-                    // currently working on the now tournaments thing in the api
-                    // need xwiki api too
+
                 })
 
         })
